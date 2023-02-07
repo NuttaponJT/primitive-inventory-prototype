7312a4ce6708b08054169ea6da398b05348fa584
nuttapon <--global>
2023-02-07 16:44:57 +0700

    update sqflite

---
+++ lib/dbs/inventory_line.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import '../models/inventory_line.dart';
+
+class InventoryLineDatabase {
+  static final InventoryLineDatabase instance = InventoryLineDatabase._init();
+
+  static Database? _database;
+
+  InventoryLineDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('inventory_line.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final textType = 'TEXT NOT NULL';
+    final integerType = 'INTEGER NOT NULL';
+
+    await db.execute('''
+CREATE TABLE $tableInventoryLine (
+  ${InventoryLineFields.id} $idType
+  , ${InventoryLineFields.item_name} $textType
+  , ${InventoryLineFields.item_desc} $textType
+  , ${InventoryLineFields.in_stock} $integerType
+)
+''');
+  }
+
+  Future<InventoryLine> create(InventoryLine inventory_line) async {
+    final db = await instance.database;
+    final id = await db.insert(tableInventoryLine, inventory_line.toJson());
+
+    return inventory_line.copy(id: id);
+  }
+
+  Future<InventoryLine> readBook(int id) async {
+    final db = await instance.database;
+    final maps = await db.query(
+      tableInventoryLine,
+      columns: InventoryLineFields.values,
+      where: '${InventoryLineFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return InventoryLine.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<InventoryLine>> readAllInventoryLine() async {
+    final db = await instance.database;
+    final orderBy = '${InventoryLineFields.id} DESC';
+    final result = await db.query(tableInventoryLine, orderBy: orderBy);
+
+    return result.map((json) => InventoryLine.fromJson(json)).toList();
+  }
+
+  Future<int> update(InventoryLine inventory_line) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableInventoryLine,
+      inventory_line.toJson(),
+      where: '${InventoryLineFields.id} = ?',
+      whereArgs: [inventory_line.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return db.delete(
+      tableInventoryLine,
+      where: '${InventoryLineFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> deleteAll() async {
+    final db = await instance.database;
+    
+    return db.delete(
+      tableInventoryLine,
+    );
+  }
+
+  Future close() async {
+    final db = await instance.database;
+
+    db.close();
+  }
+}
---
+++ lib/models/inventory_line.dart
@@ -0,0 +1,57 @@
+final String tableInventoryLine = 'inventory_line';
+
+class InventoryLineFields {
+
+  static final List<String> values = [
+    id
+    , item_name
+    , item_desc
+    , in_stock
+  ];
+
+  static final String id = '_id';
+  static final String item_name = 'item_name';
+  static final String item_desc = 'item_desc';
+  static final String in_stock = 'in_stock';
+}
+
+class InventoryLine {
+  final int? id;
+  final String item_name;
+  final String item_desc;
+  final int in_stock;
+
+  const InventoryLine({
+    this.id
+    , required this.item_name
+    , required this.item_desc
+    , required this.in_stock
+  });
+
+  InventoryLine copy({
+    int? id
+    , String? item_name
+    , String? item_desc
+    , int? in_stock
+  }) =>
+      InventoryLine(
+        id: id ?? this.id
+        , item_name: item_name ?? this.item_name
+        , item_desc: item_desc ?? this.item_desc
+        , in_stock: in_stock ?? this.in_stock
+      );
+
+  static InventoryLine fromJson(Map<String, Object?> json) => InventoryLine(
+        id: json[InventoryLineFields.id] as int?
+        , item_name: json[InventoryLineFields.item_name] as String
+        , item_desc: json[InventoryLineFields.item_desc] as String
+        , in_stock: json[InventoryLineFields.in_stock] as int
+      );
+
+  Map<String, Object?> toJson() => {
+        InventoryLineFields.id: id
+        , InventoryLineFields.item_name: item_name
+        , InventoryLineFields.item_desc: item_desc
+        , InventoryLineFields.in_stock: in_stock
+      };
+}
